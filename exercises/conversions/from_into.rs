2a689dfdb106ffa122e8c97ea6e3250b11e1f890
Augustin Peyrard <augustin.peyrard@gmail.com>
2021-02-12 21:49:19 -0800

    fix from_into

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,15 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let parts: Vec<&str> = s.split(",").collect();
+        match parts[..] {
+            [name, age] if !name.is_empty() => age.parse()
+                .map(|age| Person { name: name.to_string(), age })
+                .unwrap_or_default(),
+            _ => Default::default(),
+        }
     }
 }
 
